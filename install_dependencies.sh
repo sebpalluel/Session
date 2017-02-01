dba0f1053b8808e8d9bebfa3e1256ca5b7eda8ff
sebpalluel <sebpalluel@free.fr>
2017-02-01 10:49:09 +0100

    solve condition to update Vundle + Vim + dummy installed.dummy to check if already installed

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -11,7 +11,7 @@
 #	sed -i "" "s/psebasti/$user/g" ~/.zshrc
 #	echo "type your mail:"
 #	read mail
-#	sed -i "" "s/sebpalluel@free.fr/$mail/g" ~/.zshrc
+#	#sed -i "" "s/sebpalluel@free.fr/$mail/g" ~/.zshrc
 #fi
 
 if [ "$1" = "-42" ]; then
@@ -29,63 +29,75 @@ git config --global core.excludesfile ~/.gitignore_global
 echo "make custom script executable"
 chmod +x ~/bin/*
 
-if [ -f "~/.vim/bundle/Vundle.vim" ]; then
-echo "config Vundle"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-  else
-echo "Vundle already exist"
+if [ -f ~/.vim/bundle/Vundle.vim ]; then
+	echo "config Vundle"
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else
+	echo "Vundle already exist"
 fi
 
 echo "install python"
 if brew ls --versions python > /dev/null; then
-	  brew upgrade python
-  else
-brew install python
+	brew upgrade python
+else
+	brew install python
 fi
 
 echo "install ruby"
 if brew ls --versions ruby > /dev/null; then
-		brew upgrade ruby
-  else
-brew install ruby
+	brew upgrade ruby
+else
+	brew install ruby
 fi
 
 echo "install ghi + pygment.rb"
 if gem spec pygments.rb > /dev/null 2>&1; then
-		brew updgrade ghi
-	else
-gem install pygments.rb && brew install ghi
+	brew updgrade ghi
+else
+	gem install pygments.rb && brew install ghi
 fi
 
 echo "config mvim"
 brew tap macvim-dev/macvim
 if brew ls --versions macvim > /dev/null; then
 	brew upgrade macvim
-  else
-brew install macvim && brew linkapps macvim
+else
+	brew install macvim && brew linkapps macvim
 fi
 
 echo "config exuberant-ctags"
 if brew ls --versions ctags > /dev/null; then
-	 brew upgrade ctags
-  else
-brew install ctags
-echo "alias native ctags to exuberant-ctags"
-alias ctags="$(brew --prefix)/bin/ctags"
+	brew upgrade ctags
+else
+	brew install ctags
+	echo "alias native ctags to exuberant-ctags"
+	alias ctags="$(brew --prefix)/bin/ctags"
 fi
-ctags --exclude=@~/.ctagsignore
 
-echo "install Plugins in vim with Vundle"
-vim -c 'PluginInstall' -c 'qa!'
+if [ -f ~/.vim/installed.dummy ]; then
+	echo "update Plugins in vim with Vundle"
+	vim -c 'PluginUpdate' -c 'qa!'
+else
+	touch ~/.vim/installed.dummy
+	echo "install Plugins in vim with Vundle"
+	vim -c 'PluginInstall' -c 'qa!'
+fi
+
 
 echo "do you whant to compile with clang ?"
-echo ""
 echo "if so, type:"
 echo "y"
 read action
 if [[ $action = [Yy] ]]; then 
-echo "install C and C++ dependencies for YouCompleteMe"
-cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
+	echo "install C and C++ dependencies for YouCompleteMe"
+	cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
+fi
+
+echo "install valgrind"
+if brew ls --versions valgrind> /dev/null; then
+	brew upgrade valgrind
+else
+	brew install valgrind
 fi
 
 echo "gdb already installed and codesigned ?"
@@ -98,31 +110,31 @@ if [[ $action = [Yy] ]]; then
 fi
 
 if brew ls --versions gdb > /dev/null; then
-	 brew upgrade gdb
-  else
-brew install gdb
+	brew upgrade gdb
+else
+	brew install gdb
 fi
 
 echo "Follow those instruction to be able to use gdb on mac\n"
-echo "Start Keychain Access application 
-(/Applications/Utilities/Keychain Access.app) 
-Open menu /Keychain Access/Certificate Assistant/Create a Certificate… 
-Choose name 'gdb-cert', 
-set Identity Type to Self Signed Root, 
-set Certificate Type to Code Signing and select the Let me override defaults. 
-Click several times on Continue until you get to the Specify a Location For The Certificate screen, 
-then set Keychain to System. […] 
-Finally, using the contextual menu for the certificate, 
-select Get Info, 
-open the Trust item, 
-and set Code Signing to Always Trust. 
+echo "Start Keychain Access application
+(/Applications/Utilities/Keychain Access.app)
+Open menu /Keychain Access/Certificate Assistant/Create a Certificate…
+Choose name 'gdb-cert',
+set Identity Type to Self Signed Root,
+set Certificate Type to Code Signing and select the Let me override defaults.
+Click several times on Continue until you get to the Specify a Location For The Certificate screen,
+then set Keychain to System. […]"
+echo "Finally, using the contextual menu for the certificate,
+select Get Info,
+open the Trust item,
+and set Code Signing to Always Trust 
 You must quit Keychain Access application in order to use the certificate […]."
 echo "everythin is set ? then write 'y' and return"
 read action
 if [[ $action = [Yy] ]]; then 
-		echo "codesign gdb"
-		codesign -s gdb-cert $(brew --prefix)/bin/gdb
-		echo "you will need to reboot at the end of script in order for it to be active"
+	echo "codesign gdb"
+	codesign -s gdb-cert $(brew --prefix)/bin/gdb
+	echo "you will need to reboot at the end of script in order for it to be active"
 fi
 exit $exit_code
 exit 1
